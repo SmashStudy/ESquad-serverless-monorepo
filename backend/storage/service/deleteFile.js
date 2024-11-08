dba441489d9a9286ee5c632fa6c55af23303e76e
kimmin1kk <rudals1888@naver.com>
2024-11-08 14:56:12 +0900

    mod: s3 조작은 프론트에 위임, 함수의 역할은 dynamoDB에 존재하는 메타데이터만 삭제하게 변경

--- backend/storage/service/deleteFile.js
+++ backend/storage/service/deleteFile.js
@@ -1,20 +1,11 @@
 const AWS = require('aws-sdk');
-const s3 = new AWS.S3();
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
-const BUCKET_NAME = process.env.S3_BUCKET;
 const TABLE_NAME = process.env.METADATA_TABLE;
 
 module.exports.handler = async (event) => {
   const { storedFileName } = event.pathParameters;
 
   try {
-    // S3에서 파일 삭제
-    await s3.deleteObject({
-      Bucket: BUCKET_NAME,
-      Key: storedFileName,
-    }).promise();
-
-    // DynamoDB에서 메타데이터 삭제
     const deleteParams = {
       TableName: TABLE_NAME,
       Key: { id: storedFileName },
@@ -24,12 +15,12 @@ module.exports.handler = async (event) => {
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `File ${storedFileName} deleted successfully` }),
+      body: JSON.stringify({ message: `Metadata for ${storedFileName} deleted successfully` }),
     };
   } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: `Failed to delete file: ${error.message}` }),
+      body: JSON.stringify({ error: `Failed to delete metadata: ${error.message}` }),
     };
   }
 };
