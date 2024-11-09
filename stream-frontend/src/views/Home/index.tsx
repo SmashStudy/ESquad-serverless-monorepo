558cfb7dd75c2c56009a5062629aadcd5affef46
andle7 <hb1809@naver.com>
2024-11-10 00:29:17 +0900

    mod: 스트리밍 VersionLabel 제거

--- stream-frontend/src/views/Home/index.tsx
+++ stream-frontend/src/views/Home/index.tsx
@@ -2,12 +2,10 @@ import React from "react";
 
 import MeetingFormSelector from "../../containers/MeetingFormSelector";
 import { StyledLayout } from "./Styled";
-import { VersionLabel } from "../../utils/VersionLabel";
 
 const Home: React.FC = () => (
   <StyledLayout>
     <MeetingFormSelector />
-    <VersionLabel />
   </StyledLayout>
 );
 
