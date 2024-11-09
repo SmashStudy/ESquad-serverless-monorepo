9dd1b560f4f7a3c348f2700377fd156bc41ff0cc
andle7 <hb1809@naver.com>
2024-11-09 20:25:19 +0900

    remove: 스트리밍  service handler.js 제거

--- stream-frontend/src/meetingConfig.ts
+++ stream-frontend/src/meetingConfig.ts
@@ -2,8 +2,12 @@ import { ConsoleLogger, MultiLogger, POSTLogger } from "amazon-chime-sdk-js";
 import { SDK_LOG_LEVELS } from "./constants";
 import { MeetingConfig } from "./types";
 
+// logLevel에 사용될 가능한 값들을 명시적으로 정의합니다.
+type LogLevel = keyof typeof SDK_LOG_LEVELS; // 'debug' | 'info' | 'warn' | 'error' | 'off' 등
+
 const urlParams = new URLSearchParams(window.location.search);
-const queryLogLevel = urlParams.get("logLevel") || "info";
+const queryLogLevel: LogLevel =
+  (urlParams.get("logLevel") as LogLevel) || "info"; // 'info'가 기본값
 const logLevel = SDK_LOG_LEVELS[queryLogLevel] || SDK_LOG_LEVELS.info;
 
 const meetingConfig: MeetingConfig = {
@@ -18,6 +22,7 @@ const BASE_URL: string = [
   location.pathname.replace(/\/*$/, "/"),
 ].join("");
 
+// '0.0.0.0', '127.0.0.1', 'localhost' 제외
 if (!["0.0.0.0", "127.0.0.1", "localhost"].includes(location.hostname)) {
   const postLogger = new POSTLogger({
     url: `${BASE_URL}logs`,
