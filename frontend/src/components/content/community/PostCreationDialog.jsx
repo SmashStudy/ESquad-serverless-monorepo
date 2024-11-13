f99e1afe208aeee2b4d3d82dbd42816f4fed396c
wnsgud0310 <ffjjo0310@naver.com>
2024-11-13 11:07:53 +0900

    add: 게시글 생성 모달 생성

---
+++ frontend/src/components/content/community/PostCreationDialog.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Dialog, DialogContent } from "@mui/material";
+import PostCreationPage from "../../../pages/community/PostCreationPage.jsx";
+
+const PostCreationDialog = ({ open, onClose }) => {
+  return (
+    <Dialog open={open} onClose={onClose} maxWidth="md" fullWidth>
+      <DialogContent>
+        <PostCreationPage onCancel={onClose} />
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default PostCreationDialog;
