c6ae1a4d947d0878b87d7b3c2bc8b5baff983835
jeongmin <jeongmin0046@gmail.com>
2024-11-13 14:51:15 +0900

    mod: 로그인 코드 수정

--- backend/user/service/handler.js
+++ backend/user/service/handler.js
@@ -1,15 +1,10 @@
 // handler.js
 const { DynamoDBClient, PutItemCommand } = require('@aws-sdk/client-dynamodb');
-// const AWS = require('aws-sdk');
 const { v4: uuidv4 } = require('uuid');
 
-// Configure DynamoDB DocumentClient
 const dynamoDb = new DynamoDBClient({ region: 'us-east-1' });
-// const dynamoDb = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1' });
 
-const { jwtDecode } = require("jwt-decode");
 
-// Function to handle Cognito Post Authentication trigger and save user data to DynamoDB
 module.exports.saveUserToDynamoDB = async (event, context) => {
   try {
     const userAttributes = event.request.userAttributes;
@@ -31,7 +26,7 @@ module.exports.saveUserToDynamoDB = async (event, context) => {
 
     const command = new PutItemCommand(params);
     await dynamoDb.send(command);
-    // await dynamoDb.put(params).promise();
+  
     console.log('User saved to DynamoDB:', params.Item);
 
     return event;
@@ -41,9 +36,8 @@ module.exports.saveUserToDynamoDB = async (event, context) => {
   }
 };
 
-// Authorizer function (if needed for custom authorization logic)
 module.exports.authorizer = async (event, context) => {
-  // Example custom authorizer logic, if required
+  
   console.log('Authorization event:', event);
   return {
     principalId: 'user',
