c183d0a62d6006123ec199bf98a9dc16dd1e27a5
0237hh <153145663+0237hh@users.noreply.github.com>
2024-11-26 19:40:19 +0900

    mod: 파일 관련 필드 추가

--- backend/chatting/service/put.mjs
+++ backend/chatting/service/put.mjs
@@ -15,7 +15,7 @@ async function putItem(tableName, item) {
 
   try {
     await docClient.send(new PutCommand(params));
-    console.log("Put succeeded", item);
+    console.log("Put succeeded", JSON.stringify(item, null, 2));
   } catch (err) {
     console.error("Unable to add item. Error:", JSON.stringify(err, null, 2));
     throw err;
@@ -27,7 +27,7 @@ export const handler = async (event) => {
   console.log("Received event:", JSON.stringify(event, null, 2));
 
   const body = JSON.parse(event.body);
-  const { room_id, message, user_id, file } = body;
+  const { room_id, message, user_id, fileKey, presignedUrl, contentType, originalFileName } = body;
 
   if (!room_id || !message || !user_id) {
     return {
@@ -36,30 +36,27 @@ export const handler = async (event) => {
     };
   }
 
-  if (file && (!file.fileKey || !file.contentType || !file.originalFileName)) {
-    console.error("File metadata missing or invalid:", file);
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ error: "Missing file metadata fields for file message" }),
-    };
-  }
+  // if (fileKey && (typeof fileKey !== 'object' || !file.fileKey || !file.contentType || !file.originalFileName)) {
+  //   console.error("File metadata missing or invalid:", file);
+  //   return {
+  //     statusCode: 400,
+  //     body: JSON.stringify({ error: "Missing file metadata fields for file message" }),
+  //   };
+  // }
 
   const now = moment().valueOf();
   const item = {
-    room_id : String(room_id),
+    room_id: String(room_id),
     timestamp: now,
     message: message || null,
-    user_id,
-    ...(file && {
-      fileKey: file.fileKey,
-      contentType: file.contentType,
-      originalFileName: file.originalFileName,
-    }),
+    user_id: String(user_id),
+    isFile: fileKey ? true : false, // 파일 여부 플래그 추가
+    fileKey: fileKey ? fileKey?.S : null,
+    presignedUrl: fileKey ? presignedUrl : null,
+    contentType: fileKey ? contentType : null,
+    originalFileName: fileKey ? originalFileName : null,
   };
-
-  if (file) {
-    console.log("Storing file metadata:", file);
-  }
+  console.log("Attempting to put item in DynamoDB:", JSON.stringify(item, null, 2));
 
   // DynamoDB에 메시지 PUT 하기
   try {
