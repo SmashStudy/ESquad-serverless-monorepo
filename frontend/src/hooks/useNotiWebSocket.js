9425b13bf37e3091949e4749067511050b845ad0
yejeii <98299007+yejeii@users.noreply.github.com>
2024-11-27 12:03:32 +0900

    mod: 알림 통신 앤드포인트 수정

--- frontend/src/hooks/useNotiWebSocket.js
+++ frontend/src/hooks/useNotiWebSocket.js
@@ -1,62 +1,65 @@
 import { useEffect, useRef } from "react";
 
-const domainName = 'ws.noti.api.esquad.click';
+// const domainName = 'ws.noti.api.esquad.click';
+const domainName = "cjf00kxsf3.execute-api.us-east-1.amazonaws.com/local";
 const useWebSocket = (userId, onMessageReceived) => {
-    const socketRef = useRef(null);
+  const socketRef = useRef(null);
 
-    useEffect(() => {
-        const connectToWebSocket = () => {
-            if (
-                socketRef.current &&
-                (socketRef.current.readyState === WebSocket.OPEN ||
-                    socketRef.current.readyState === WebSocket.CONNECTING)
-            ) {
-                console.log("WebSocket is already active. Skipping new connection.");
-                return;
-            }
+  useEffect(() => {
+    const connectToWebSocket = () => {
+      if (
+        socketRef.current &&
+        (socketRef.current.readyState === WebSocket.OPEN ||
+          socketRef.current.readyState === WebSocket.CONNECTING)
+      ) {
+        console.log("WebSocket is already active. Skipping new connection.");
+        return;
+      }
 
-            const address = `wss:/${domainName}?userId=${encodeURIComponent(userId)}`;
-            const ws = new WebSocket(address);
-            console.log("Creating a new WebSocket connection.");
+      const address = `wss://${domainName}?userId=${encodeURIComponent(
+        userId
+      )}`;
+      const ws = new WebSocket(address);
+      console.log("Creating a new WebSocket connection.");
 
-            ws.onopen = () => {
-                console.log("WebSocket 연결 성공");
-                const fetchNotificationsMessage = JSON.stringify({
-                    action: "countUnReadNotifications",
-                    userId: userId,
-                });
-                ws.send(fetchNotificationsMessage);
-            };
+      ws.onopen = () => {
+        console.log("WebSocket 연결 성공");
+        const fetchNotificationsMessage = JSON.stringify({
+          action: "countUnReadNotifications",
+          userId: userId,
+        });
+        ws.send(fetchNotificationsMessage);
+      };
 
-            ws.onmessage = (message) => {
-                const obj = JSON.parse(message.data);
-                console.log(`Received messages from websocket: ${JSON.stringify(obj)}`);
-                onMessageReceived(obj);
-            };
+      ws.onmessage = (message) => {
+        const obj = JSON.parse(message.data);
+        console.log(`Received messages from websocket: ${JSON.stringify(obj)}`);
+        onMessageReceived(obj);
+      };
 
-            ws.onclose = () => {
-                console.log("WebSocket 연결 종료");
-                socketRef.current = null;
-            };
+      ws.onclose = () => {
+        console.log("WebSocket 연결 종료");
+        socketRef.current = null;
+      };
 
-            ws.onerror = (event) => {
-                console.error("WebSocket 에러 발생:", event);
-                socketRef.current = null;
-            };
+      ws.onerror = (event) => {
+        console.error("WebSocket 에러 발생:", event);
+        socketRef.current = null;
+      };
 
-            socketRef.current = ws;
-        };
+      socketRef.current = ws;
+    };
 
-        connectToWebSocket();
+    connectToWebSocket();
 
-        return () => {
-            if (socketRef.current) {
-                console.log("Closing WebSocket connection.");
-                socketRef.current.close();
-                socketRef.current = null;
-            }
-        };
-    }, [userId, onMessageReceived]);
+    return () => {
+      if (socketRef.current) {
+        console.log("Closing WebSocket connection.");
+        socketRef.current.close();
+        socketRef.current = null;
+      }
+    };
+  }, [userId, onMessageReceived]);
 };
 
 export default useWebSocket;
