035ae101079d87117924c56b6a8ec96f3a511ef4
yejeii <98299007+yejeii@users.noreply.github.com>
2024-11-28 02:26:43 +0900

    refactor: 알림 관련 API , util 컴포넌트화

--- frontend/src/hooks/notificationAPI.js
+++ frontend/src/hooks/notificationAPI.js
@@ -2,74 +2,129 @@ import axios from "axios";
 
 const API_GATEWAY_URL = "https://api.esquad.click/local/notification";
 
-export const fetchNotifications = async (
-  userId,
-  lastEvaluatedKey,
-  showArchived
-) => {
-  const endpoint = showArchived
-    ? `${API_GATEWAY_URL}/filter-saved`
-    : `${API_GATEWAY_URL}/all`;
+export const fetchAll = async ({ lastEvaluatedKey = null, user }) => {
+  try {
+    const endpoint = `${API_GATEWAY_URL}/all`;
 
-  const response = await axios.post(
-    endpoint,
-    {
-      userId,
-      lastEvaluatedKey,
-    },
-    {
-      headers: {
-        "Content-Type": "application/json",
+    const response = await axios.post(
+      endpoint,
+      {
+        userId: user.email,
+        lastEvaluatedKey, // Include pagination key if provided
       },
-    }
-  );
+      {
+        headers: { "Content-Type": "application/json" },
+      }
+    );
 
-  return response.data;
+    return response.data;
+  } catch (error) {
+    throw "처리에 실패했습니다.";
+  }
 };
 
-export const markNotificationsAsRead = async (userId, notificationIds) => {
-  const response = await axios.post(
-    `${API_GATEWAY_URL}/mark`,
-    {
-      userId,
-      notificationIds,
-    },
-    {
-      headers: {
-        "Content-Type": "application/json",
+export const fetchAllSaved = async ({ user, key }) => {
+  try {
+    const response = await axios.post(
+      `${API_GATEWAY_URL}/filter-saved`,
+      {
+        userId: user.email,
+        ...(key && { lastEvaluatedKey: key }),
       },
-    }
-  );
-
-  return response;
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    console.log(`Saved notifications: ${JSON.stringify(response.data)}`);
+    return response.data;
+  } catch (err) {
+    throw "처리에 실패했습니다.";
+  }
 };
 
-export const markNotificationAsSave = async (userId, id) => {
-  const response = await axios.get(
-    `${API_GATEWAY_URL}/save?userId=${encodeURIComponent(
-      userId
-    )}&id=${encodeURIComponent(id)}`,
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
+export const markAllAsRead = async ({
+  notifications,
+  user,
+  setNotifications,
+  setUnReadCount,
+}) => {
+  const notificationIds = notifications
+    .filter((notification) => notification.isRead === "0") // Filter unread notifications
+    .map((notification) => notification.id); // Extract IDs
 
-  return response.data;
+  if (notificationIds.length > 0) {
+    try {
+      const response = await axios.post(
+        `${API_GATEWAY_URL}/mark`,
+        { notificationIds, userId: user.email },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (response.status === 200) {
+        setNotifications((prev) =>
+          prev.map((notification) =>
+            notification.isRead === "1"
+              ? notification
+              : { ...notification, isRead: "1" }
+          )
+        );
+        setUnReadCount((prevCount) =>
+          Math.max(prevCount - notificationIds.length, 0)
+        );
+      }
+    } catch (error) {
+      throw "처리에 실패했습니다.";
+    }
+  } else {
+    alert("모든 알림이 모두 읽음 처리된 상태입니다!");
+  }
 };
 
-export const releaseSaveNotification = async (userId, id) => {
-  const response = await axios.get(
-    `${API_GATEWAY_URL}/release-save?userId=${encodeURIComponent(
-      userId
-    )}&id=${encodeURIComponent(id)}`,
-    {
+export const markAsSave = async ({ user, notificationId }) => {
+  try {
+    const url = `${API_GATEWAY_URL}/save`;
+    const queryParams = `?userId=${encodeURIComponent(
+      user.email
+    )}&id=${encodeURIComponent(notificationId)}`;
+    const fullUrl = `${url}${queryParams}`;
+
+    const response = await axios.get(fullUrl, {
       headers: {
         "Content-Type": "application/json",
       },
-    }
-  );
+    });
 
-  return response.data;
+    if (response.status === 200) {
+      return response.data;
+    }
+  } catch (error) {
+    throw "알림 처리에 실패했습니다.";
+  }
+};
+
+export const releaseSaved = async ({ user, notificationId }) => {
+  try {
+    const url = `${API_GATEWAY_URL}/release-save`;
+    const queryParams = `?userId=${encodeURIComponent(
+      user.email
+    )}&id=${encodeURIComponent(notificationId)}`;
+    const fullUrl = `${url}${queryParams}`;
+
+    const response = await axios.get(fullUrl, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.status === 200) {
+      return response.data;
+    }
+  } catch (error) {
+    throw "알림 저장 해제에 실패했습니다.";
+  }
 };
--- frontend/src/hooks/useNotiWebSocket.js
+++ frontend/src/hooks/useNotiWebSocket.js
@@ -1,65 +1,68 @@
 import { useEffect, useRef } from "react";
 
-// const domainName = 'ws.noti.api.esquad.click';
-const domainName = "cjf00kxsf3.execute-api.us-east-1.amazonaws.com/local";
-const useWebSocket = (userId, onMessageReceived) => {
+/**
+ * Custom hook for managing a WebSocket connection.
+ * @param {Function} onMessageReceived - Callback to handle incoming WebSocket messages.
+ * @param {Object} options - Optional configurations.
+ * @param {Function} setUnReadCount - Function to update unread count state.
+ * @param {Function} setNotifications - Function to update notifications state.
+ */
+
+const useNotiWebSocket = ({ user, onMessageReceived }) => {
+  // const address = `wss://ws.noti.api.esquad.click?userId=${encodeURIComponent(
+  //   user.email
+  // )}`;
+  const address = `wss://cjf00kxsf3.execute-api.us-east-1.amazonaws.com/local?userId=${encodeURIComponent(
+    user.email
+  )}`;
   const socketRef = useRef(null);
 
+  const connectToWebSocket = () => {
+    if (
+      socketRef.current &&
+      (socketRef.current.readyState === WebSocket.OPEN ||
+        socketRef.current.readyState === WebSocket.CONNECTING)
+    ) {
+      console.log("WebSocket is already active. Skipping new connection.");
+      return;
+    }
+
+    const ws = new WebSocket(address);
+    console.log("Creating a new WebSocket connection.");
+
+    ws.onopen = () => {
+      console.log("WebSocket 연결 성공");
+      const fetchNotificationsMessage = JSON.stringify({
+        action: "countUnReadNotifications",
+        userId: user.email,
+      });
+      ws.send(fetchNotificationsMessage);
+    };
+
+    ws.onmessage = (message) => {
+      const obj = JSON.parse(message.data);
+      console.log(`Received messages from websocket: ${JSON.stringify(obj)}`);
+      onMessageReceived(obj);
+    };
+
+    ws.onclose = () => {
+      console.log("WebSocket 연결 종료");
+      socketRef.current = null;
+    };
+
+    ws.onerror = (event) => {
+      console.error("WebSocket 에러 발생:", event);
+      socketRef.current = null;
+    };
+
+    socketRef.current = ws;
+  };
+
   useEffect(() => {
-    const connectToWebSocket = () => {
-      if (
-        socketRef.current &&
-        (socketRef.current.readyState === WebSocket.OPEN ||
-          socketRef.current.readyState === WebSocket.CONNECTING)
-      ) {
-        console.log("WebSocket is already active. Skipping new connection.");
-        return;
-      }
+    connectToWebSocket(); // Connect WebSocket on mount
+  }, []);
 
-      const address = `wss://${domainName}?userId=${encodeURIComponent(
-        userId
-      )}`;
-      const ws = new WebSocket(address);
-      console.log("Creating a new WebSocket connection.");
-
-      ws.onopen = () => {
-        console.log("WebSocket 연결 성공");
-        const fetchNotificationsMessage = JSON.stringify({
-          action: "countUnReadNotifications",
-          userId: userId,
-        });
-        ws.send(fetchNotificationsMessage);
-      };
-
-      ws.onmessage = (message) => {
-        const obj = JSON.parse(message.data);
-        console.log(`Received messages from websocket: ${JSON.stringify(obj)}`);
-        onMessageReceived(obj);
-      };
-
-      ws.onclose = () => {
-        console.log("WebSocket 연결 종료");
-        socketRef.current = null;
-      };
-
-      ws.onerror = (event) => {
-        console.error("WebSocket 에러 발생:", event);
-        socketRef.current = null;
-      };
-
-      socketRef.current = ws;
-    };
-
-    connectToWebSocket();
-
-    return () => {
-      if (socketRef.current) {
-        console.log("Closing WebSocket connection.");
-        socketRef.current.close();
-        socketRef.current = null;
-      }
-    };
-  }, [userId, onMessageReceived]);
+  return { connectToWebSocket };
 };
 
-export default useWebSocket;
+export default useNotiWebSocket;
