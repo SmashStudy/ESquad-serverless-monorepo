933a3560845857c13808b8669c5df8c715bc2f78
kimmin1kk <rudals1888@naver.com>
2024-11-29 01:55:01 +0900

    chore: 스테이지별 엔드포인트 변경 자동화

--- frontend/src/utils/apiConfig.js
+++ frontend/src/utils/apiConfig.js
@@ -1,10 +1,11 @@
-export const getStorageApi = () => 'https://api.esquad.click/dev/files';
-export const getUserApi = () => 'https://api.esquad.click/dev/users';
-export const getChatApi = () => 'https://api.esquad.click/dev/chat';
-export const getCommunityApi = () => 'https://api.esquad.click/dev/community';
-export const getNotificationApi = () => 'https://api.esquad.click/dev/notification'
-export const getTeamApi = () => 'https://api.esquad.click/teams'
-export const getStreamApi = () => 'https://api.esquad.click/dev/stream'
+const environment = import.meta.env.VITE_ENVIRONMENT || 'local';
 
-// export const getNotificationWebsocketApi = () => 'wss://cjf00kxsf3.execute-api.us-east-1.amazonaws.com/local'
-export const getNotificationWebsocketApi = () => 'wss://ws.noti.api.esquad.click'
+export const getStorageApi = () => `https://api.esquad.click/${environment}/files`;
+export const getUserApi = () => `https://api.esquad.click/${environment}/users`;
+export const getChatApi = () => `https://api.esquad.click/${environment}/chat`;
+export const getCommunityApi = () => `https://api.esquad.click/${environment}/community`;
+export const getNotificationApi = () => `https://api.esquad.click/${environment}/notification`
+export const getTeamApi = () => 'https://api.esquad.click/teams'
+export const getStreamApi = () => `https://api.esquad.click/${environment}/stream`
+
+export const getNotificationWebsocketApi = () => environment === 'local' ? 'wss://cjf00kxsf3.execute-api.us-east-1.amazonaws.com/local' : 'wss://ws.noti.api.esquad.click'
