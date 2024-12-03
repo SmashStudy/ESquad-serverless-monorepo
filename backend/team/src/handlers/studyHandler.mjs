3f5495e6456d45159cf83a038e776e475b5f29ab
wive10 <70935978+wive10@users.noreply.github.com>
2024-12-03 15:18:14 +0900

    mod: team id 파라미터에서 받기

--- backend/team/src/handlers/studyHandler.mjs
+++ backend/team/src/handlers/studyHandler.mjs
@@ -1,14 +1,17 @@
 // studyHandler.mjs
 
 import { StudyService } from "../services/studyService.mjs";
+import { BookService } from "../services/bookService.mjs";
 
 const studyService = new StudyService();
+const bookService = new BookService();
 
-export const createStudyHandler = async (event) => {
+export const createStudy = async (event) => {
     try {
-        const { bookDto, teamId, studyInfo } = JSON.parse(event.body);
+        const teamId = event.pathParameters?.teamId;
+        const { bookDto, studyData } = JSON.parse(event.body);
         const bookId = await bookService.saveBook(bookDto);
-        const studyPageId = await studyService.createStudyPage(teamId, bookId, studyInfo);
+        const studyPageId = await studyService.createStudy(teamId, bookId, studyData);
         return createResponse(201, { message: "Study page created successfully", studyPageId });
     } catch (error) {
         console.error("Error in createStudyHandler:", error.message);
