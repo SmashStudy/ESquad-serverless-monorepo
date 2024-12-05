eae81ad026f9a8363edc882f2da0239bf5001567
andle7 <hb1809@naver.com>
2024-12-05 14:17:32 +0900

    mod: 스트리밍 teamId 추가로 받아 올 수 있도록 수정

--- frontend/src/pages/stream/LiveStreamWindow.jsx
+++ frontend/src/pages/stream/LiveStreamWindow.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { CardActions, Button } from "@mui/material";
 import { getStreamingApi } from "../../utils/apiConfig";
 
-const LiveStreamWindow = ({ nickname, studyId }) => {
+const LiveStreamWindow = ({ teamId, studyId, nickname }) => {
   const handleStreamingButtonClick = (e) => {
     // 이벤트 전파 방지
     e.stopPropagation();
 
     // URL을 정상적인 형식으로 수정
-    const popupUrl = `${getStreamingApi()}?studyId=${studyId}&name=${encodeURIComponent(nickname)}`;
+    const popupUrl = `${getStreamingApi()}?teamId=${teamId}&studyId=${studyId}&name=${encodeURIComponent(nickname)}`;
 
     // 팝업을 열고 URL 파라미터를 전달
     window.open(popupUrl, "_blank", "width=900,height=700,scrollbars=yes,resizable=yes");
