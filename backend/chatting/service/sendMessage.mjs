ef2bc4988a7c45881403f7b9253348785b5580e9
0237hh <153145663+0237hh@users.noreply.github.com>
2024-12-06 17:07:07 +0900

    mod: nickname 컬럼 추가

--- backend/chatting/service/sendMessage.mjs
+++ backend/chatting/service/sendMessage.mjs
@@ -25,6 +25,7 @@ export const handler = async (event) => {
             timestamp: Date.now(),
             message,
             user_id,
+            nickname,
         };
 
         if (fileKey) {
