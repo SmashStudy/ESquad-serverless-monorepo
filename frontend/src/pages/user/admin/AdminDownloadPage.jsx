e47724f4ef02e6d906b1356596c2226fc6de725c
kimmin1kk <rudals1888@naver.com>
2024-12-06 17:04:00 +0900

    feat: 어드민 파일 관리 기능 추가

--- frontend/src/pages/user/admin/AdminDownloadPage.jsx
+++ frontend/src/pages/user/admin/AdminDownloadPage.jsx
@@ -1,14 +1,134 @@
-import React from "react";
-import { Typography, Box } from "@mui/material";
+import React, {useEffect, useState} from "react";
+import {
+  Typography,
+  Box,
+  useTheme,
+  Container, CircularProgress, Grid
+} from "@mui/material";
+import {formatFileSize} from "../../../utils/fileFormatUtils.js";
+import axios from "axios";
+import {getStorageApi} from "../../../utils/apiConfig.js";
+import {AgGridReact} from "ag-grid-react";
 
 const AdminDownloadPage = () => {
+  const [logs, setLogs] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const theme = useTheme();
+
+  useEffect(() => {
+    fetchLogs();
+  }, []);
+
+  const columnDefs = [
+    {
+      headerName: '로그 PK',
+      field: 'logId',
+      sortable: true,
+      filter: true
+    },
+    {
+      headerName: '이벤트 발생 시점',
+      field: 'createdAt',
+      sortable: true,
+      filter: true
+    },
+    {
+      headerName: '파일 키',
+      field: 'fileKey',
+      sortable: true,
+      filter: true
+    },
+    {
+      headerName: '접근 IP',
+      field: 'ipAddress',
+      sortable: true,
+      filter: true
+    },
+    {
+      headerName: '파일 이름',
+      field: 'originalFileName',
+      sortable: true,
+      filter: true
+    },
+    {
+      headerName: '타겟 PK',
+      field: 'targetId',
+      sortable: true,
+      filter: true
+    },
+    {
+      headerName: '타겟 유형',
+      field: 'targetType',
+      sortable: true,
+      filter: true
+    },
+    {
+      headerName: '생성한 유저',
+      field: 'uploaderEmail',
+      sortable: true,
+      filter: true
+    },
+    {
+      headerName: '유저 에이전트',
+      field: 'userAgent',
+      sortable: true,
+      filter: true
+    },
+    {
+      headerName: '접근한 유저',
+      field: 'userEmail',
+      sortable: true,
+      filter: true
+    },
+    {headerName: '역할', field: 'userRole', sortable: true, filter: true},
+    {
+      headerName: '파일 크기',
+      field: 'fileSize',
+      sortable: true,
+      filter: true,
+      valueFormatter: ({value}) => formatFileSize(value)
+    }
+  ];
+
+  const fetchLogs = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(`${getStorageApi()}/admin/logs/DOWNLOAD`);
+
+      const data = await response.data.items;
+      setLogs(data);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+      setLoading(false);
+    }
+  };
+
+  if (loading) {
+    return (
+        <Container style={{textAlign: "center", marginTop: "20%"}}>
+          <CircularProgress style={{marginBottom: "20px"}}/>
+          <Typography variant="h6">파일 정보를 불러오는 중입니다...</Typography>
+        </Container>
+    );
+  }
+
   return (
-    <Box>
-      <Typography variant="h6" style={{ textAlign: "center", marginBottom: "20px" }}>
-        다운로드 관리 페이지
-      </Typography>
-      {/* Add download management functionality here */}
-    </Box>
+      <Grid item xs={10}>
+        <Box sx={{height: '100%'}}>
+          <div className="ag-theme-alpine"
+               style={{width: '100%'}}>
+            <AgGridReact
+                rowData={logs}
+                columnDefs={columnDefs}
+                pagination={true}
+                paginationPageSize={10}
+                paginationPageSizeSelector={[10, 20, 50, 100]}
+                domLayout="autoHeight"
+            />
+          </div>
+        </Box>
+      </Grid>
   );
 };
 
--- frontend/src/pages/user/admin/AdminFilePage.jsx
+++ frontend/src/pages/user/admin/AdminFilePage.jsx
@@ -1,14 +1,179 @@
-import React from "react";
-import { Typography, Box } from "@mui/material";
+import React, {useEffect, useState} from "react";
+import {
+  Typography,
+  Box,
+  Container,
+  CircularProgress,
+  TextField,
+  IconButton,
+  Tooltip,
+  Button,
+  TableContainer,
+  Paper,
+  Table,
+  TableHead, TableRow, TableCell, TableBody, Chip, Grid, useTheme
+} from "@mui/material";
+import {getStorageApi, getUserApi} from "../../../utils/apiConfig.js";
+import SearchIcon from "@mui/icons-material/Search";
+import RefreshIcon from "@mui/icons-material/Refresh";
+import SortIcon from "@mui/icons-material/Sort";
+import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
+import PersonAddAltIcon from "@mui/icons-material/PersonAddAlt";
+import {
+  handleFileDelete,
+  handleFileDownload
+} from "../../../utils/storage/utilities.js";
+import DownloadIcon from "@mui/icons-material/Download";
+import DeleteIcon from "@mui/icons-material/Delete";
+import {formatFileSize} from "../../../utils/fileFormatUtils.js";
+import axios from "axios";
+import {AgGridReact} from "ag-grid-react";
 
 const AdminFilePage = () => {
+  const [files, setFiles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const theme = useTheme();
+
+  useEffect(() => {
+    fetchFiles();
+  }, []);
+
+  const columnDefs = [
+    {
+      headerName: '작업',
+      field: 'actions',
+      cellStyle: { display: 'flex', justifyContent: 'center', alignItems: 'center' }, // 버튼 가운데 정렬
+      cellRenderer: (params) => {
+        const { fileKey, originalFileName, userEmail } = params.data;
+        return (
+            <Box sx={{ display: 'flex', gap: 1 }}> {/* 버튼 간격 조정 */}
+              <Tooltip title="다운로드">
+                <IconButton
+                    aria-label="download"
+                    size="small"
+                    onClick={() => handleFileDownload(fileKey, originalFileName, setSnackbar)}
+                    sx={{ color: theme.palette.success.main }}
+                >
+                  <DownloadIcon />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="삭제">
+                <IconButton
+                    aria-label="delete"
+                    size="small"
+                    onClick={() => handleFileDelete(fileKey, userEmail, userEmail, setSnackbar, fetchData)}
+                    sx={{ color: theme.palette.warning.main }}
+                >
+                  <DeleteIcon />
+                </IconButton>
+              </Tooltip>
+            </Box>
+        );
+      }
+    },
+    {
+      headerName: '파일 이름',
+      field: 'originalFileName',
+      sortable: true,
+      filter: true
+    },
+    {
+      headerName: '다운로드 수',
+      field: 'downloadCount',
+      sortable: true,
+      filter: true
+    },
+    {
+      headerName: '파일 크기',
+      field: 'fileSize',
+      sortable: true,
+      filter: true,
+      valueFormatter: ({value}) => formatFileSize(value)
+    },
+    {headerName: '올린 시간', field: 'createdAt', sortable: true, filter: true},
+    {headerName: '확장자', field: 'extension', sortable: true, filter: true},
+    {
+      headerName: '업로더',
+      field: 'userEmail',
+      sortable: true,
+      filter: true
+    },
+    {
+      headerName: '유저 닉네임',
+      field: 'userNickname',
+      sortable: true,
+      filter: true
+    },
+    {
+      headerName: '타겟 PK',
+      field: 'targetId',
+      sortable: true,
+      filter: true
+    },
+    {
+      headerName: '타겟 유형',
+      field: 'targetType',
+      sortable: true,
+      filter: true
+    },
+    {
+      headerName: '콘텐츠 유형',
+      field: 'contentType',
+      sortable: true,
+      filter: true
+    },
+    {
+      headerName: '파일 PK',
+      field: 'fileKey',
+      sortable: true,
+      filter: true
+    }
+
+  ];
+
+
+
+
+  const fetchFiles = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(`${getStorageApi()}/admin/metadata`);
+
+      const data = await response.data;
+      setFiles(data);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+      setLoading(false);
+    }
+  };
+
+
+  if (loading) {
+    return (
+        <Container style={{ textAlign: "center", marginTop: "20%" }}>
+          <CircularProgress style={{ marginBottom: "20px" }} />
+          <Typography variant="h6">파일 정보를 불러오는 중입니다...</Typography>
+        </Container>
+    );
+  }
+
   return (
-    <Box>
-      <Typography variant="h6" style={{ textAlign: "center", marginBottom: "20px" }}>
-        파일 관리 페이지
-      </Typography>
-      {/* Add file management functionality here */}
-    </Box>
+      <Grid item xs={10}>
+        <Box sx={{height: '100%'}}>
+          <div className="ag-theme-alpine"
+               style={{width: '100%'}}>
+            <AgGridReact
+                rowData={files}
+                columnDefs={columnDefs}
+                pagination={true}
+                paginationPageSize={10}
+                paginationPageSizeSelector={[10, 20, 50, 100]}
+                domLayout="autoHeight"
+            />
+          </div>
+        </Box>
+      </Grid>
   );
 };
 
