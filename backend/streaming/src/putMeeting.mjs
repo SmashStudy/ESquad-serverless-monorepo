e1c7851f049539e8585b42de0aae20222871ae89
andle7 <hb1809@naver.com>
2024-12-07 20:04:39 +0900

    mod: 스트리밍 참여자 이름 컬럼 추가 수정

--- backend/streaming/src/putMeeting.mjs
+++ backend/streaming/src/putMeeting.mjs
@@ -13,7 +13,7 @@ const MeetingRoomUsageTrackingTableName = process.env.MEETING_ROOM_USAGE_TABLE_N
  * @returns {void}
  * @throws {Error} - DynamoDB 오류 발생 시
  */
-export const putMeeting = async (title, meetingInfo, userEmail, teamId, status ) => {
+export const putMeeting = async (title, attendeeName, meetingInfo, userEmail, teamId, status ) => {
  
   const now = new Date();
   const ttlSeconds = Math.floor(now.getTime() / 1000) + 3 * 60 * 60;
@@ -35,8 +35,9 @@ export const putMeeting = async (title, meetingInfo, userEmail, teamId, status )
           Put: {
             TableName: MeetingRoomUsageTrackingTableName,
             Item: {
-              teamId: {S: teamId},
+              teamId: {S: teamId },
               title: { S: title },
+              name: { S: attendeeName },
               userEmail: { S: userEmail },
               start_At: { S: now.toISOString() },
               status: { S: status },
